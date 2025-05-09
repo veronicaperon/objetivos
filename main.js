d1cb953100de4bb212d27a27fae6138d0400b50e
veronicaperon <160857545+veronicaperon@users.noreply.github.com>
2025-05-09 08:07:59 -0300

    .

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ for(let i=0; i<botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-03-05T00:00:00");
-const tempoObjetivo2 = new Date("2025-09-05T00:00:00");
+const tempoObjetivo1 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-09-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-05T00:00:00");
-const tempoObjetivo4 = new Date("202-02-05T00:00:00");
+const tempoObjetivo4 = new Date("2027-02-05T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
